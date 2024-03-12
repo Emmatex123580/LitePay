e3ed14a213e81e6f844e658910a0ba1ae872ebf7
Emmatex123580 <113904522+Emmatex123580@users.noreply.github.com>
2024-03-12 20:04:16 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/scheduler.dart';
+import 'core/app_export.dart';
+import 'core/size_utils.dart';
+import 'routes/app_routes.dart';
+import 'theme/theme_helper.dart';
+
+var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  ///Please update theme as per your need if required.
+  ThemeHelper().changeTheme('primary');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return MaterialApp(
+          theme: theme,
+          title: 'litepay',
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppRoutes.signInScreen,
+          routes: AppRoutes.routes,
+        );
+      },
+    );
+  }
+}
